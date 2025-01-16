05fac28c6373f90d00cd6f058633f6043a9b39c5
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-01-16 15:54:35 +0530

    main

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,99 +1,99 @@
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
-    try {
-      const { email, fullName, password, profilePic } = req.body;
-      if (!fullName || !email || !password || !profilePic) {
-        return res.status(400).json({
-          success: false,
-          message: "Please provide all fields",
-        });
-      }
+  try {
+    const { email, fullName, password, profilePic } = req.body;
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide all fields",
+      });
+    }
 
-      if ( password < 6 ) {
-        return res.status(400).json({
-          success: false,
-          message: "Password should be at least 6 characters long",
-        });
-      }
-  
-      const user = await User.findOne({ email });
-      if (user) {
-        return res.status(400).json({
-          success: false,
-          message: "User already exists",
-        });
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-      await User.create({
-        fullName,
-        email,
-        password: hashedPassword,
-        profilePic,
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password should be at least 6 characters long",
       });
-      return res.status(201).json({
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      profilePic,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide all fields",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .json({
         success: true,
-        message: "User created successfully",
+        message: `Welcome, ${user.fullName}`,
       });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
-  
-  export const login = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      if (!email || !password) {
-        return res.status(400).json({
-          success: false,
-          message: "Please provide all fields",
-        });
-      }
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(400).json({
-          success: false,
-          message: "User does not exist",
-        });
-      }
-      const passwordMatch = await bcrypt.compare(password, user.password);
-      if (!passwordMatch) {
-        return res.status(400).json({
-          success: false,
-          message: "Invalid credentials",
-        });
-      }
-  
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "1h",
-      });
-      return res
-        .status(200)
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "strict",
-          maxAge: 24 * 60 * 60 * 1000,
-        })
-        .json({
-          success: true,
-          message: `Welcome, ${user.fullName}`,
-        });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
-  
-  export const logout = (_, res) => {
-      try {
-          return res.status(200).cookie("token", "", { maxAge: 0 }).json({
-              success: true,
-              message: "Logged out successfully",
-          });
-      } catch (error) {
-          console.log(error);
-      }
-  };
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (_, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
