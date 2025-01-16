9e1a369f00411b32cc32dc314c23a5c636f11f23
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-01-16 16:12:38 +0530

    main

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.route.js';
 
@@ -9,8 +10,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes)
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
