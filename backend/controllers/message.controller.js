84fb2b66cc7900f95378dfa6d2dd3813a4e70bdf
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-01-22 22:51:00 +0530

    main

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import Message from "../models/message.model";
 import User from "../models/user.model";
 
 export const getUserForSidebar = async (req, res) => {
@@ -19,12 +20,65 @@ export const getUserForSidebar = async (req, res) => {
 };
 
 export const getMessages = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        res.status(404).json({
-            success: false,
-            message: error.message,
-        });
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.params._id;
+    const messages = await Message.find({
+      $or: [
+        {
+          senderId: myId,
+          receiverId: userToChatId,
+        },
+        {
+          senderId: userToChatId,
+          receiverId: myId,
+        },
+      ],
+    });
+    res.status(200).json({
+      success: true,
+      messages,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    if (!text &&!image) {
+      return res.status(400).json({
+        message: "Text or image is required",
+      });
     }
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+        senderId,
+        receiverId,
+        text,
+        image: imageUrl,
+    })
+
+    await newMessage.save();
+    res.status(201).json({
+      success: true,
+      message: "Message sent successfully",
+      newMessage,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
--- backend/routes/message.auth.js
+++ backend/routes/message.auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { protectRoutes } from '../middleware/auth.middleware.js';
-import { getMessages, getUserForSidebar } from '../controllers/message.controller.js';
+import { getMessages, getUserForSidebar, sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 // Define routes
 router.get('/users', protectRoutes, getUserForSidebar)
 router.get('/:id', protectRoutes, getMessages)
+router.post('/send/:id', protectRoutes, sendMessage)
 
 export default router;
