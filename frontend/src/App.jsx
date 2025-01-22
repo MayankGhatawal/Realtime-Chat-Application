052bec8d3194117af3827ee214d5630511836d33
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-01-23 00:11:23 +0530

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import Signup from "./pages/Signup"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Setting from "./pages/Setting"
+import Profile from "./pages/Profile"
 
 function App() {
 
   return (
     <>
-      <p className="text-bold">hello guys</p>
+      <Routes>
+      <Route path="/" exact elements={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/setting" exact element={<Setting />} />
+      <Route path="/profile/:userId" exact element={<Profile />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="/signup" exact element={<Signup />} />
+      </Routes>
     </>
   )
 }
