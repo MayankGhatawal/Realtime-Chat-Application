15e9c1667df5f9413cda5a238a4c3208efd58741
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-01-23 11:51:56 +0530

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,44 @@
-import { Route, Routes } from "react-router-dom"
+import { createBrowserRouter, Route, Routes } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Setting from "./pages/Setting"
-import Profile from "./pages/Profile"
+import Profile from "./pages/profileInfo"
+import { RouterProvider } from "react-router-dom"
+import NotFound from "./pages/NotFound"
 
 function App() {
 
-  return (
-    <>
-      <Routes>
-      <Route path="/" exact elements={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/setting" exact element={<Setting />} />
-      <Route path="/profile/:userId" exact element={<Profile />} />
-      {/* <Route path="*" element={<NotFound />} /> */}
-      <Route path="/signup" exact element={<Signup />} />
-      </Routes>
-    </>
-  )
+  // Implementing Protected Routes
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/setting",
+      element: <Setting />,
+    },
+    {
+      path: "/profile/:userId",
+      element: <Profile />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ])
+  
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
--- frontend/src/pages/profileInfo.jsx
+++ frontend/src/pages/profileInfo.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function profileInfo() {
   return (
     <div>profileInfo</div>
