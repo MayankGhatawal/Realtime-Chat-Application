33cf600c4d0b07e374d2a6320d01915d70c0b62d
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-01-23 12:50:14 +0530

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Loader } from 'lucide-react';
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,23 +11,27 @@ import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect } from "react";
 
 function App() {
-  const { authUser, checkAuth } = useAuthStore()
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log({authUser});
+  if (isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
   
   return (
     <>
     <Navbar />
     <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" exact element={authUser ? <Home /> : <Navigate to="/login" />} />
+      <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/" />} />
+      <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
       <Route path="/setting" element={<Setting />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login" />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
     </>
